4529cc3e843a7dc06a53cb255448787452835d41
Ubuntu <ubuntu@ip-172-31-18-10.us-west-2.compute.internal>
2016-06-16 04:03:20 +0000

    chartist demo

--- chartist/index.js
+++ chartist/index.js
@@ -1,7 +1,5 @@
 var data = {
-  // A labels array that can contain any sort of values
   labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
   series: [
     [5, 2, 4, 2, 0]
   ]
@@ -12,9 +10,6 @@ var options = {
   width: window.innerWidth/2+'px'
 };
 
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
 new Chartist.Bar('#bar', data, options);
 new Chartist.Line('#line', data, options);
 
